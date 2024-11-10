cadbad11b02a923465bb7b89a268d0990b10a319
guiireal <guii_real@hotmail.com>
2024-11-10 14:55:46 -0300

    chore: new files

---
+++ config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+
+// Prefixo dos comandos.
+exports.PREFIX = "/";
+
+// Emoji do bot (mude se preferir).
+exports.BOT_EMOJI = "🤖";
+
+// Nome do bot (mude se preferir).
+exports.BOT_NAME = "LITE BOT";
+
+// Número do bot. Coloque o número do bot (apenas números).
+exports.BOT_NUMBER = "5511920202020";
+
+// Número do dono do bot. Coloque o número do dono do bot (apenas números).
+exports.OWNER_NUMBER = "5511999999999";
+
+// Diretório de arquivos de mídia.
+exports.ASSETS_DIR = path.resolve(__dirname, "assets");
+
+// Diretório de arquivos temporários.
+exports.TEMP_DIR = path.resolve(__dirname, "temp");
+
+// Timeout em milissegundos por evento (evita banimento).
+exports.TIMEOUT_IN_MILLISECONDS_BY_EVENT = 700;
+
+// Plataforma de API's
+exports.SPIDER_API_BASE_URL = "https://api.spiderx.com.br/api";
+
+// Obtenha seu token, criando uma conta em: https://api.spiderx.com.br.
+exports.SPIDER_API_TOKEN = "seu_token_aqui";
---
+++ index.js
@@ -0,0 +1,3 @@
+const { connect } = require("./connection");
+
+(async () => {})();
