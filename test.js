18dffdab755c7fe1decb9cd4815a09b2fd5b1b66
guiireal <guii_real@hotmail.com>
2024-11-24 13:08:58 -0300

    wip

--- test.js
+++ test.js
@@ -5,5 +5,16 @@
  * @author Dev Gui
  */
 (async () => {
-  console.log("Apenas testando");
+  const fetchJson = async (url) => {
+    const response = await fetch(url);
+    return await response.json();
+  };
+
+  const q = "https://www.youtube.com/watch?v=Oz7pHJ65gjM";
+
+  const api = await fetchJson(
+    ` http://br3.bronxyshost.com:4237/youtube/playmp3?q=${q}&apikey=igoBots`
+  );
+
+  console.log(api);
 })();
